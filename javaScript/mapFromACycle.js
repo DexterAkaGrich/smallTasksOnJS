a6d85ebfe8d59e04bc5e1110c295107c3eafbbb0
Dmitry <veni4ek18@gmail.com>
2019-02-03 07:30:29 +0200

    map from a cycle

---
+++ javaScript/mapFromACycle.js
@@ -0,0 +1,20 @@
+//Given the following code example that needs to be rewritten via the MapS method
+// var arr = ["Есть", "жизнь", "на", "Марсе"];
+//
+// var arrLength = [];
+// for (var i = 0; i < arr.length; i++) {
+//   arrLength[i] = arr[i].length;
+// }
+//
+// alert( arrLength ); // 4,5,2,5
+
+
+//Solution
+var arr = ["Есть", "жизнь", "на", "Марсе"];
+
+var arrLength = arr.map(function (arr){
+  return arr.length
+})
+
+alert( arrLength )
+//Similar code looks much simpler and clearer for the reader
